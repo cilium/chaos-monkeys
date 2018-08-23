a5987b766460c802cba44696031e7f9561e6d127
Thomas Graf <thomas@cilium.io>
2018-08-23 02:04:36 +0200

    New breed of E.T. monkey with FQDN policies
    Signed-off-by: Thomas Graf <thomas@cilium.io>

---
+++ monkeys/et-call-home-fqdn-policy/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+. $(dirname ${BASH_SOURCE})/helpers.bash
+
+init_monkey
+
+[ -z "$URL" ] && {
+	echo "URL not provided"
+	exit 1
+}
+
+[ -z "$SLEEP" ] && {
+	SLEEP="10"
+}
+
+endpoint_debug_enable
+
+while :
+do
+	start_monitor
+	start_tcpdump
+
+	echo -n "Trying to call home ($URL): "
+
+	time curl -4 -s $CURL_OPTIONS 10 $URL > /dev/null
+	CODE=$?
+	echo "$CODE"
+
+	stop_monitor
+	stop_tcpdump
+
+	[ "$CODE" -ne "0" ] && {
+		notify_slack ":fire: *E.T. ($HOSTNAME) cannot call home!* (exit code $CODE) :face_palm:"
+		test_fail
+		exit 1
+	}
+
+	sleep $SLEEP
+done
