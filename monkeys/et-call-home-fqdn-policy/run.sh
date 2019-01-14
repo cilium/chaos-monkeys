f7a997cc6981891e5392da5205935558295242bc
Eloy Coto <eloy.coto@gmail.com>
2019-01-14 15:02:16 +0100

    Monkeys: Added localport on curl output.
    To be able to debug easily print the local port used by curl
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- monkeys/et-call-home-fqdn-policy/run.sh
+++ monkeys/et-call-home-fqdn-policy/run.sh
@@ -5,37 +5,37 @@
 init_monkey
 
 [ -z "$URL" ] && {
-	echo "URL not provided"
-	exit 1
+    echo "URL not provided"
+    exit 1
 }
 
 [ -z "$SLEEP" ] && {
-	SLEEP="10"
+    SLEEP="10"
 }
 
 endpoint_debug_enable
 
 while :
 do
-	start_monitor
-	start_tcpdump
+    start_monitor
+    start_tcpdump
 
-	echo -n "Trying to call 1.1.1.1: "
-	time curl -vvv -I  -w "dns: %{time_namelookup} connect: %{time_connect} total: %{time_total}\n" -4 -s $CURL_OPTIONS -o /dev/null 1.1.1.1 
+    echo -n "Trying to call 1.1.1.1: "
+    time curl -vvv -I  -w "dns: %{time_namelookup} connect: %{time_connect} total: %{time_total}\n" -4 -s $CURL_OPTIONS -o /dev/null 1.1.1.1
 
-	echo -n "Trying to call home ($URL): "
-	time curl -vvv -I -w "dns: %{time_namelookup} connect: %{time_connect} total: %{time_total}\n" -4 -s $CURL_OPTIONS -o /dev/null $URL
-	CODE=$?
-	echo "$CODE"
+    echo -n "Trying to call home ($URL): "
+    time curl -vvv -I -w "dns: %{time_namelookup}(%{remote_ip}) connect: %{time_connect} total: %{time_total} localPort: %{local_port}\n" -4 -s $CURL_OPTIONS -o /dev/null $URL
+    CODE=$?
+    echo "$CODE"
 
-	stop_monitor
-	stop_tcpdump
+    stop_monitor
+    stop_tcpdump
 
-	[ "$CODE" -ne "0" ] && {
-		notify_slack ":fire: *E.T. ($HOSTNAME) cannot call home!* (exit code $CODE) :face_palm:"
-		test_fail
-		exit 1
-	}
+    [ "$CODE" -ne "0" ] && {
+        notify_slack ":fire: *E.T. ($HOSTNAME) cannot call home!* (exit code $CODE) :face_palm:"
+        test_fail
+        exit 1
+    }
 
-	sleep $SLEEP
+    sleep $SLEEP
 done
